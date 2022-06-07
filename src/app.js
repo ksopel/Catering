3b080398640bdb7f847ef6dffb250e544b8f5335
ksopel <katarzyna.sopel@outlook.com>
2022-06-07 11:24:25 +0200

    changes

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import * as ReactDOM from 'react-dom';
+
+function App () {
+    return (
+        <>
+          <div>
+
+        </div>
+            </>
+    )
+}
+
+
+const container = document.getElementById('app');
+const root = ReactDOM.createRoot(container);
+root.render(<App/>);
